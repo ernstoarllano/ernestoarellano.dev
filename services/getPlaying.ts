4dbbd24daef6c12507b3378aeb26bebc638ede91
Ernesto Arellano <ernestoarellano@icloud.com>
2022-08-03 21:55:53 -0700

    add sentry and update spotify services to use try catch

--- services/getPlaying.ts
+++ services/getPlaying.ts
@@ -3,31 +3,35 @@ import { spotifyToken } from 'lib/spotify'
 const endpoint = 'https://api.spotify.com/v1/me/player/currently-playing'
 
 export const getPlaying = async () => {
-  const { access_token } = await spotifyToken()
+  try {
+    const { access_token } = await spotifyToken()
 
-  const res = await fetch(endpoint, {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  })
+    const res = await fetch(endpoint, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    })
 
-  if (res.status === 204 || res.status > 400) {
-    return false
-  }
+    if (res.status === 204 || res.status > 400) {
+      return false
+    }
 
-  const { item } = await res.json()
+    const { item } = await res.json()
 
-  if (item === null) {
-    return false
-  }
+    if (item === null) {
+      return false
+    }
 
-  const name = item.name
-  const artist = item.artists.map((_artist: any) => _artist.name).join(', ')
-  const duration = item.duration_ms
+    const name = item.name
+    const artist = item.artists.map((_artist: any) => _artist.name).join(', ')
+    const duration = item.duration_ms
 
-  return {
-    name,
-    artist,
-    duration,
+    return {
+      name,
+      artist,
+      duration,
+    }
+  } catch (err) {
+    throw err
   }
 }
