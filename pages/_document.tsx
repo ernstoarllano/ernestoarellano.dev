4dbe664f8c1be3a4d302fe0ee98fde08ec21675c
Ernesto Arellano <ernestoarellano@icloud.com>
2022-08-12 18:10:02 -0700

    fix google tag placement

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,20 +17,21 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-9ZD4HEVBST"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        <Head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-9ZD4HEVBST"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-            gtag('config', 'G-9ZD4HEVBST');
-          `}
-        </Script>
+              gtag('config', 'G-9ZD4HEVBST');
+            `}
+          </Script>
+        </Head>
         <body>
           <Main />
           <NextScript />
