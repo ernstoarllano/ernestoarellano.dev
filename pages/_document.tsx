bd341c5d078465429355123f6aaab7d417ea189d
Ernesto Arellano <ernestoarellano@icloud.com>
2022-08-12 17:52:12 -0700

    add google analytics tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -17,6 +18,19 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9ZD4HEVBST"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-9ZD4HEVBST');
+          `}
+        </Script>
         <body>
           <Main />
           <NextScript />
