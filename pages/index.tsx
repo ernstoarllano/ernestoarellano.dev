8323ec02df9bf03a1fe6a7a53d86e7706e9f3dc3
Ernesto Arellano <ernestoarellano@icloud.com>
2022-08-20 12:03:17 -0700

    remove blog for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import About from 'components/about/Intro'
 import Resume from 'components/about/Resume'
-import Blog from 'components/blog/Blog'
 import Timeline from 'components/experience/Timeline'
 import Footer from 'components/Footer'
 import Repos from 'components/github/Repos'
@@ -14,22 +13,9 @@ import { HomePageProps } from 'interfaces/interfaces'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { getPage } from 'services/getPage'
-import { getPosts } from 'services/getPosts'
 import { getRepos } from 'services/getRepos'
-import useSWR from 'swr'
-import { fetcher } from 'utils/fetcher'
 
-const HomePage = ({ content, repos, posts }: HomePageProps) => {
-  const { data: fetchedContent } = useSWR('/api/page/1', fetcher, {
-    fallbackData: content,
-  })
-  const { data: fetchedRepos } = useSWR('/api/repos', fetcher, {
-    fallbackData: repos,
-  })
-  const { data: fetchedPosts } = useSWR('/api/posts', fetcher, {
-    fallbackData: posts,
-  })
-
+const HomePage = ({ content, repos }: HomePageProps) => {
   const { width } = useWindowSize()
   const isDesktop = width >= 1024
 
@@ -41,10 +27,9 @@ const HomePage = ({ content, repos, posts }: HomePageProps) => {
       <Header />
       <Hero />
       <main className="space-y-24 lg:space-y-48">
-        <About content={fetchedContent} />
+        <About content={content} />
         <Timeline jobs={jobs} />
-        <Repos repos={fetchedRepos} />
-        <Blog posts={fetchedPosts} />
+        <Repos repos={repos} />
         <Footer />
       </main>
       <Profiles profiles={profiles} />
@@ -57,13 +42,11 @@ export const getStaticProps: GetStaticProps = async () => {
   try {
     const { content } = await getPage(1)
     const { repos } = await getRepos()
-    const { posts } = await getPosts()
 
     return {
       props: {
         content,
         repos,
-        posts,
       },
       revalidate: 43200,
     }
