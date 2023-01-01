d66ea9002ed6e637bcca853f6704fd5243b8c65b
Ernesto Arellano <ernestoarellano@icloud.com>
2023-01-01 12:33:52 -0700

    add vercel analytics package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/500.css'
 import '@fontsource/poppins/600.css'
 import '@fontsource/poppins/700.css'
+import { Analytics } from '@vercel/analytics/react'
 import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app'
 import 'styles/globals.css'
@@ -11,6 +12,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <PlausibleProvider domain="ernestoarellano.dev">
       <Component {...pageProps} />
+      <Analytics />
     </PlausibleProvider>
   )
 }
