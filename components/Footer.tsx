0c9118766cd313e5b507ed02846ffcf7d1733424
Ernesto Arellano <ernestoarellano@icloud.com>
2023-02-27 16:44:27 -0700

    resolve errors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,4 @@
 export default function Footer() {
-  const year = new Date().getFullYear()
-
   return (
     <footer className="py-12 text-xs text-sandstone text-center">
       &copy; Ernesto Arellano
